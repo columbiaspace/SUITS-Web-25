a440a1d55447192eb0088b0b56dba2c6c228a29f
GitGoblr <jhegstad12@gmail.com>
2025-01-20 21:20:02 -0500

    removed ec2 stuff... moving to aws lightsail!

--- config/gunicorn.conf.py
+++
@@ -1,3 +0,0 @@
-bind = "unix:/tmp/gunicorn.sock"
-workers = 3
-wsgi_app = "wsgi:app"
--- gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-bind = "0.0.0.0:8000"
-workers = 3
-worker_class = "eventlet"
-timeout = 120
-keepalive = 5
-errorlog = "gunicorn-error.log"
-accesslog = "gunicorn-access.log"
-loglevel = "info" 
